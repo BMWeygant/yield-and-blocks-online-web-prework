1fef12be306b92a41d9f83ee9702c6b80811a919
BRANDON M WEYGANT <bobbydigital108@gmail.com>
2019-02-05 04:55:32 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,16 @@
 def hello_t(array)
-array.each do |name|
-  puts "Hi, #{name}"
-end
+  if block_given?
+      i = 0
+
+      while i < array.length
+        yield(array[i])
+        i = i + 1
+      end
+
+      array
+    else
+      puts "Hey! No block was given!"
+    end
 end
 
 # call your method here!
